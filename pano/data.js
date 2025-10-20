a6829e99dd25dc9045ef75a02d0030bdc7e3064d
shkartmaz <sashakh@live.ru>
2025-10-20 17:09:55 +0300

    initial

---
+++ pano/data.js
@@ -0,0 +1,79 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano_20250924_111942_0",
+      "name": "PANO_20250924_111942_0",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.2295779246854135,
+        "pitch": 0.27622475345370034,
+        "fov": 1.3848081938437478
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.12979769623892778,
+          "pitch": 0.27355712610508043,
+          "rotation": 0,
+          "target": "1-pano_20250924_111942_0"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pano_20250924_111942_0",
+      "name": "PANO_20250924_111942_0",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9616369963651383,
+          "pitch": 0.47572902931307404,
+          "rotation": 0,
+          "target": "0-pano_20250924_111942_0"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
